eb2c6047f3233b6c2b63c0b5d1e5d0e38d1d4fb8
Ildefonso Lares <65493114+Flayxamax@users.noreply.github.com>
2023-06-20 21:09:56 -0700

    Index finalizado

--- views/slide-lulu.js
+++ views/slide-lulu.js
@@ -1,7 +1,7 @@
 var bannerImg = document.querySelector('.banner .imagen-lulu');
-var startPosition = bannerImg.offsetHeight; // La altura de la imagen
+var startPosition = bannerImg.offsetHeight;
 var currentPosition = startPosition;
-var targetPosition = startPosition / 2; // La mitad de la altura de la imagen
+var targetPosition = startPosition / 2;
 
 function slideImage() {
   currentPosition -= 5;
@@ -24,7 +24,7 @@ window.onload = function() {
   setTimeout(function() {
     slide.style.transition = "all 0.8s ease-out";
     slide.style.opacity = "1";
-    slide.style.display = "flex"; // Agregamos esta línea para utilizar el modelo de diseño flexible
+    slide.style.display = "flex";
     startSlide();
   }, 500);
 };
